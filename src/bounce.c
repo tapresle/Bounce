6982da95047761a143fc49a0a8e3947e7253e8c1
Ted Presley <tapresle@ncsu.edu>
2014-07-01 19:24:23 -0400

    Initial Commit

---
+++ src/bounce.c
@@ -0,0 +1,203 @@
+#include "pebble.h"
+
+#define MATH_PI 3.141592653589793238462
+#define NUM_DISCS 6
+#define DISC_DENSITY 0.25
+#define ACCEL_RATIO 0.05
+#define ACCEL_STEP_MS 50
+
+typedef struct Vec2d {
+  double x;
+  double y;
+} Vec2d;
+
+typedef struct Disc {
+  Vec2d pos;
+  Vec2d vel;
+  double mass;
+  double radius;
+} Disc;
+
+static Disc discs[NUM_DISCS];
+
+static double next_radius = 8;
+
+static Window *window;
+
+static GRect window_frame;
+
+static Layer *disc_layer;
+
+static AppTimer *timer;
+
+TextLayer *text_time_layer;
+
+TextLayer *bat_layer;
+
+static void handle_battery(BatteryChargeState charge_state) {
+  static char battery_text[] = "100%";
+  snprintf(battery_text, sizeof(battery_text), "%d%%", charge_state.charge_percent);
+  
+  text_layer_set_text(bat_layer, battery_text);
+}
+
+void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  // Need to be static because they're used by the system later.
+  static char time_text[] = "00:00";
+
+  char *time_format;
+
+
+
+  if (clock_is_24h_style()) {
+    time_format = "%R";
+  } else {
+    time_format = "%I:%M";
+  }
+
+  strftime(time_text, sizeof(time_text), time_format, tick_time);
+
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_text[0] == '0')) {
+    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+  }
+
+  text_layer_set_text(text_time_layer, time_text);
+  
+  handle_battery(battery_state_service_peek());
+}
+
+static double disc_calc_mass(Disc *disc) {
+  return MATH_PI * disc->radius * disc->radius * DISC_DENSITY;
+}
+
+static void disc_init(Disc *disc) {
+  GRect frame = window_frame;
+  disc->pos.x = frame.size.w/2;
+  disc->pos.y = frame.size.h/2;
+  disc->vel.x = 0;
+  disc->vel.y = 0;
+  disc->radius = next_radius;
+  disc->mass = disc_calc_mass(disc);
+  next_radius += 1;
+}
+
+static void disc_apply_force(Disc *disc, Vec2d force) {
+  disc->vel.x += force.x / disc->mass;
+  disc->vel.y += force.y / disc->mass;
+}
+
+static void disc_apply_accel(Disc *disc, AccelData accel) {
+  Vec2d force;
+  force.x = accel.x * ACCEL_RATIO;
+  force.y = -accel.y * ACCEL_RATIO;
+  disc_apply_force(disc, force);
+}
+
+static void disc_update(Disc *disc) {
+  const GRect frame = window_frame;
+  double e = 0.5;
+  if ((disc->pos.x - disc->radius < 0 && disc->vel.x < 0)
+    || (disc->pos.x + disc->radius > frame.size.w && disc->vel.x > 0)) {
+    disc->vel.x = -disc->vel.x * e;
+  }
+  if ((disc->pos.y - disc->radius < 0 && disc->vel.y < 0)
+    || (disc->pos.y + disc->radius > frame.size.h && disc->vel.y > 0)) {
+    disc->vel.y = -disc->vel.y * e;
+  }
+  disc->pos.x += disc->vel.x;
+  disc->pos.y += disc->vel.y;
+}
+
+static void disc_draw(GContext *ctx, Disc *disc) {
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, GPoint(disc->pos.x, disc->pos.y), disc->radius);
+}
+
+static void disc_layer_update_callback(Layer *me, GContext *ctx) {
+  for (int i = 0; i < NUM_DISCS; i++) {
+    disc_draw(ctx, &discs[i]);
+  }
+}
+
+static void timer_callback(void *data) {
+  AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
+
+  accel_service_peek(&accel);
+
+  for (int i = 0; i < NUM_DISCS; i++) {
+    Disc *disc = &discs[i];
+    disc_apply_accel(disc, accel);
+    disc_update(disc);
+  }
+
+  layer_mark_dirty(disc_layer);
+
+  timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect frame = window_frame = GRect(0,40,144,126);
+
+  disc_layer = layer_create(frame);
+  layer_set_update_proc(disc_layer, disc_layer_update_callback);
+  layer_add_child(window_layer, disc_layer);
+
+  for (int i = 0; i < NUM_DISCS; i++) {
+    disc_init(&discs[i]);
+  }
+  
+  text_time_layer = text_layer_create(GRect(2, 2, 144-2, 168-120));
+  text_layer_set_text_color(text_time_layer, GColorWhite);
+  text_layer_set_background_color(text_time_layer, GColorClear);
+  text_layer_set_font(text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HV_32)));
+  layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
+  
+  bat_layer = text_layer_create(GRect(75, 2, /* width */ 149, 168 /* height */));
+  text_layer_set_text_color(bat_layer, GColorWhite);
+  text_layer_set_background_color(bat_layer, GColorClear);
+  text_layer_set_font(bat_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HV_32)));
+  text_layer_set_text_alignment(bat_layer, GTextAlignmentLeft);
+  text_layer_set_text(bat_layer, "");
+  layer_add_child(window_layer, text_layer_get_layer(bat_layer));
+
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  battery_state_service_subscribe(&handle_battery);
+  // TODO: Update display here to avoid blank display on launch?
+  time_t now = time(NULL);
+  struct tm *current_time = localtime(&now);
+  handle_minute_tick(current_time, MINUTE_UNIT);
+}
+
+static void window_unload(Window *window) {
+  layer_destroy(disc_layer);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload
+  });
+  window_stack_push(window, true /* Animated */);
+  window_set_background_color(window, GColorBlack);
+
+  accel_data_service_subscribe(0, NULL);
+
+  timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
+}
+
+static void deinit(void) {
+  accel_data_service_unsubscribe();
+
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
